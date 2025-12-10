6b65b05e3981d6f2af49a726103d42e785ced574
ducbang1310 <ducb1310@gmail.com>
2025-12-10 13:46:28 +0700

    ffff

--- QuanLyKhachSan/GUI/CheckOut.Designer.cs
+++ QuanLyKhachSan/GUI/CheckOut.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CheckOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(816, 474);
+            this.Name = "CheckOut";
             this.Text = "CheckOut";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
